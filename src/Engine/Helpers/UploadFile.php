0866ad1b5cecb7d5ea39b3d5d9a8950a9f8c941d
Jan Dolata <j.dolata@holonglobe.com>
2016-05-24 12:32:03 +0200

    move WithFileTrait method logic to helper/UploadFile

---
+++ src/Engine/Helpers/UploadFile.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace JanDolata\CrudeCRUD\Helpers;
+
+use JanDolata\CrudeCRUD\Engine\Models\FileLog;
+
+class CrudeFiles
+{
+
+    /**
+     * Upload files for model, without save model
+     * @param  Model $model      - with 'files' attribute
+     * @param  string $folderName
+     * @param  Files $files
+     * @return Model
+     */
+    public function upload($model, $folderName, $files)
+    {
+        $updatedFiles = $model->files;
+
+        foreach ($files as $file) {
+
+            $log = (new FileLog)->create([
+                'model_id' => $id,
+                'model_name' => $folderName,
+                'file_original_name' => $file->getClientOriginalName(),
+            ]);
+
+            $systemFileName = $this->createSystemFileName($log, $file);
+            $filePath = $this->createFilePath($folderName, $systemFileName);
+            $folderPath = $this->createFilePath($folderName);
+
+            Storage::put(
+                $filePath,
+                file_get_contents($file->path())
+            );
+
+            $log->fill([
+                'file_path' => $filePath,
+                'file_system_name' => $systemFileName
+            ])->save();
+
+            $updatedFiles[] = [
+                'file_log_id' => $log->id,
+                'path' => asset($log->file_path),
+                'file_original_name' => $log->file_original_name
+            ];
+        }
+
+        $model->files = $updatedFiles;
+        return $model;
+    }
+
+    /**
+     * Delete file for model, without save model
+     * @param  Model $model
+     * @param  FileLog $log
+     * @return Model
+     */
+    public function delete($model, FileLog $log)
+    {
+        Storage::delete($log->file_path);
+
+        $updatedFiles = [];
+        foreach ($model->files as $file) {
+            if ($file['file_log_id'] == $log->id) continue;
+
+            $updatedFiles[] = $file;
+        }
+
+        $log->delete();
+
+        $model->files = $updatedFiles;
+        return $model;
+    }
+
+    /**
+     * Create file path
+     * @param  string $folderName
+     * @param  string $fileName = ''
+     * @return string
+     */
+    private function createFilePath($folderName, $fileName = '')
+    {
+        return config('crude.uploadFolder') . '/' . Str::lower($folderName) . '/' . $fileName;
+    }
+
+    /**
+     * Create system file name
+     * @param  FileLog $log
+     * @param  File    $file
+     * @return string
+     */
+    private function createSystemFileName(FileLog $log, $file)
+    {
+        return $log->id . '_' . time() . '.' . $file->getClientOriginalExtension();
+    }
+}
--- src/Engine/Traits/WithFileTrait.php
+++ src/Engine/Traits/WithFileTrait.php
@@ -3,6 +3,7 @@
 namespace JanDolata\CrudeCRUD\Engine\Traits;
 
 use JanDolata\CrudeCRUD\Engine\Models\FileLog;
+use JanDolata\CrudeCRUD\Helpers\CrudeFiles;
 use Illuminate\Support\Str;
 use Storage;
 
@@ -19,38 +20,9 @@ trait WithFileTrait
     {
         $crudeName = $this->getCalledClassName();
         $model = $this->getById($id);
-        $filesArray = $model->files;
+        $model = (new CrudeFiles)->upload($model, $crudeName, $files);
 
-        foreach ($files as $file) {
-
-            $log = (new FileLog)->create([
-                'model_id' => $id,
-                'model_name' => $crudeName,
-                'file_original_name' => $file->getClientOriginalName(),
-            ]);
-
-            $systemFileName = $this->createSystemFileName($log, $file);
-            $filePath = $this->createFilePath($crudeName, $systemFileName);
-            $folderPath = $this->createFilePath($crudeName);
-
-            Storage::put(
-                $filePath,
-                file_get_contents($file->path())
-            );
-
-            $log->fill([
-                'file_path' => $filePath,
-                'file_system_name' => $systemFileName
-            ])->save();
-
-            $filesArray[] = [
-                'file_log_id' => $log->id,
-                'path' => asset($log->file_path),
-                'file_original_name' => $log->file_original_name
-            ];
-        }
-
-        return $this->updateById($id, ['files' => $filesArray]);
+        return $this->updateById($id, ['files' => $model->files]);
     }
 
     /**
@@ -61,41 +33,9 @@ trait WithFileTrait
      */
     public function deleteFileByFileLog(FileLog $log)
     {
-        Storage::delete($log->file_path);
-
         $model = $this->getById($log->model_id);
+        $model = (new CrudeFiles)->delete($model, $log);
 
-        $updatedFiles = [];
-        foreach ($model->files as $file) {
-            if ($file['file_log_id'] == $log->id) continue;
-
-            $updatedFiles[] = $file;
-        }
-
-        $log->delete();
-
-        return $this->updateById($log->model_id, ['files' => $updatedFiles]);
-    }
-
-    /**
-     * Create file path
-     * @param  string $crudeName
-     * @param  string $fileName = ''
-     * @return string
-     */
-    private function createFilePath($crudeName, $fileName = '')
-    {
-        return config('crude.uploadFolder') . '/' . Str::lower($crudeName) . '/' . $fileName;
-    }
-
-    /**
-     * Create system file name
-     * @param  FileLog $log
-     * @param  File    $file
-     * @return string
-     */
-    private function createSystemFileName(FileLog $log, $file)
-    {
-        return $log->id . '_' . time() . '.' . $file->getClientOriginalExtension();
+        return $this->updateById($log->model_id, ['files' => $model->files]);
     }
 }
