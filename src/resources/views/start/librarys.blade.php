bda048288a5fb2413e35a481eedd8f21e7c41143
Jan Dolata <j.dolata@holonglobe.com>
2016-06-15 18:38:45 +0200

    start partials

---
+++ src/resources/views/start/librarys.blade.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="{{ asset('vendor/jan-dolata/crude-crud/css/app.css') }}">
+<script src="{{ asset('vendor/jan-dolata/crude-crud/js/lib.js') }}"></script>
+<script src="{{ asset('vendor/jan-dolata/crude-crud/js/app.js') }}"></script>
