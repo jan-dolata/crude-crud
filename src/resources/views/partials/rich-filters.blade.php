ef46c43b74e7a08231aa918e4bd55650e85d85c3
Jan Dolata <j.dolata@holonglobe.com>
2017-01-27 22:07:25 +0100

    rich filters

--- src/resources/views/partials/rich-filters.blade.php
+++ src/resources/views/partials/rich-filters.blade.php
@@ -1,11 +1,15 @@
 <div class="form-inline">
 
+    <button type="button" id="useRichFilters" class="btn btn-default btn-sm pull-right m-xs-t">
+        <i class="fa fa-search"></i>
+    </button>
+
     <%
     var setupRichFilters = setup.get('richFilters');
     for(var i in setupRichFilters) {
         var setupRichFilter = setupRichFilters[i];
     %>
-        <div class="input-group">
+        <div class="input-group m-xs-b m-xs-t">
             <span class="input-group-addon">
                 <%- setupRichFilter.label %>
             </span>
@@ -26,9 +30,5 @@
             </div>
         </div>
     <% } %>
-
-    <button type="button" id="useRichFilters" class="btn btn-default btn-sm">
-        <i class="fa fa-search"></i>
-    </button>
 
 </div>
