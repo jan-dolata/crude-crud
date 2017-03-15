1c3f423e92f7f49845b7b486ff086ef21b1c1a8e
Jan Dolata <j.dolata@holonglobe.com>
2017-03-15 22:14:23 +0100

    fix in special_files partials

--- src/resources/views/special-files/label-with-info.blade.php
+++ src/resources/views/special-files/label-with-info.blade.php
@@ -2,7 +2,7 @@
     <span class="p">
         {{ $file['label'] }}
     </span>
-    <small>
+    <small class="m-l">
         .{{ $file['data']['extension'] }};
         {{ number_format((int) $file['data']['size'] / 1024, 2, '.', ' ') }} KB;
         {{ date('Y-m-d H:i:s', $file['data']['lastModified']) }};
@@ -12,4 +12,7 @@
     <span class="p bg-danger">
         {{ $file['label'] }}
     </span>
+    <small class="m-l">
+        #{{ $file['key']  }}
+    </small>
 @endif
--- src/resources/views/special-files/list.blade.php
+++ src/resources/views/special-files/list.blade.php
@@ -4,15 +4,15 @@
     <div class="crude-box container p-lg-t p-lg-b">
         @foreach ($crudeSpecialFiles as $key => $file)
             <div class="row">
-                <div class="col-sm-8">
+                <div class="col-sm-7">
                     <div class="p">
                         @include('CrudeCRUD::special-files.download-btn', ['file' => $file])
                         @include('CrudeCRUD::special-files.label-with-info', ['file' => $file])
                     </div>
                 </div>
 
-                <div class="col-sm-4">
-                    @include('CrudeCRUD::special-files.upload-form')
+                <div class="col-sm-5">
+                    @include('CrudeCRUD::special-files.upload-form', ['key' => $key])
                 </div>
             </div>
         @endforeach
